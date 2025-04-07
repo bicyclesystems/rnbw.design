380ed49e4f5db4c9cef8aab7cb72b77abdbf61c4
Eden Vidal <edenvidal@gmail.com>
2025-04-07 16:59:06 +0300

    Refactor footer integration: Replace under-footer with rnbw-footer in multiple HTML files and update script sources to use local rnbw-footer.js for consistency and improved performance.

--- js/rnbw-header.js
+++ js/rnbw-header.js
@@ -1,20 +1,17 @@
 const headerTemplate = `
 <nav class="box-l padding-l gap-l justify-stretch" id="rnbw-logo">
     <div class="gap-l box">
-        <a href="https://weareunder.design/" target="_blank" class="under-logo">
-        <svg-icon src="https://rnbw.design/images/under.svg" style="width: 2.5rem; height: 3.5rem;"></svg-icon>
-        </a>
         <a href="https://rnbw.design/" class="rnbw-logo">
         <svg-icon src="https://rnbw.design/images/rnbw.svg"></svg-icon>
         </a>
         <div class="text-s">high-quality,</br> design and</br>development tools</div>
     </div>
     <div class="gap-m justify-end box">
-        <a id="nav-item" href="https://rnbw.dev" class="align-center column text-m gap-xs">
+        <a href="https://rnbw.dev" id="nav-item" class="align-center column text-m gap-xs">
         <svg-icon src="https://rnbw.design/images/logo.svg" id="header-item"></svg-icon>
             rnbw 0.1
         </a>
-        <a id="nav-item" href="https://renecss.org" class="align-center column text-m gap-xs">
+        <a href="https://renecss.org" id="nav-item" class="align-center column text-m gap-xs">
         <svg-icon src="https://rnbw.design/images/rene.svg" id="header-item"></svg-icon>
         rene.css
         </a>
@@ -29,21 +26,8 @@ class RnbwHeader extends HTMLElement {
   }
 
   connectedCallback() {
-    this.setupLogoHovers();
     this.setupNavItemHovers();
     this.highlightActiveLink();
-  }
-
-  setupLogoHovers() {
-    const underLogo = this.querySelector('.under-logo svg-icon');
-
-    // Under logo hover
-    underLogo.parentElement.addEventListener('mouseover', () => {
-      underLogo.setAttribute('src', 'https://rnbw.design/images/under-hover.svg');
-    });
-    underLogo.parentElement.addEventListener('mouseout', () => {
-      underLogo.setAttribute('src', 'https://rnbw.design/images/under.svg');
-    });
   }
 
   setupNavItemHovers() {
@@ -51,8 +35,10 @@ class RnbwHeader extends HTMLElement {
 
     navItems.forEach((navItem) => {
       const headerItem = navItem.querySelector("#header-item");
+      if (!headerItem) return;
+      
       const originalSrc = headerItem.getAttribute('src');
-      const hoverSrc = originalSrc.replace('.svg', '-hover.svg'); // Replace with your hover state naming convention
+      const hoverSrc = originalSrc.replace('.svg', '-hover.svg');
 
       navItem.addEventListener("mouseover", () => {
         headerItem.setAttribute('src', hoverSrc);
@@ -65,18 +51,14 @@ class RnbwHeader extends HTMLElement {
   }
 
   highlightActiveLink() {
-    function isLinkActive(link) {
-      return link.href === window.location.href;
-    }
-
+    const currentUrl = window.location.href;
+    
     this.querySelectorAll("#nav-item").forEach((navItem) => {
-      const link = navItem.querySelector("a");
-      if (isLinkActive(link)) {
-        navItem.querySelector("svg-icon").classList.add("foreground-secondary");
+      if (navItem.href === currentUrl) {
+        navItem.querySelector("#header-item").classList.add("foreground-secondary");
       }
     });
   }
-  
 }
 
 customElements.define("rnbw-header", RnbwHeader);
